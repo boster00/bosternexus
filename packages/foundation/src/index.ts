bcd421a781b046ecc55e11f798cab9acfefc48bf
boster00 <boster00@users.noreply.github.com>
2025-12-13 18:23:03 -0800

    Clean workspace: Remove all source code for security-first regeneration

--- packages/foundation/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './errors/AppError';
-export * from './result/Result';
-export * from './logger/Logger';
-export * from './external-api/ExternalApiClient';
-export * from './idempotency/IdempotencyStore';
-
--- packages/shared/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './search/SearchDTO';
-export * from './transaction/TransactionDTO';
-
